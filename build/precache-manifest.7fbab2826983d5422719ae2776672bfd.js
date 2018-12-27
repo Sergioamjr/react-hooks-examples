0e359017ee8e1710c34e336d659d7dd34a9ada24
Sergioamjr <sergioamjr91@gmail.com>
2018-12-27 20:41:34 -0200

    publish build folder

---
+++ build/precache-manifest.7fbab2826983d5422719ae2776672bfd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8c02c945d3cde1394a0c",
+    "url": "/static/js/main.8c02c945.chunk.js"
+  },
+  {
+    "revision": "a55d103718c38975aaa3",
+    "url": "/static/js/1.a55d1037.chunk.js"
+  },
+  {
+    "revision": "8c02c945d3cde1394a0c",
+    "url": "/static/css/main.6974d9e0.chunk.css"
+  },
+  {
+    "revision": "aeaa3f8bf68405572f9a09a204360326",
+    "url": "/index.html"
+  }
+];
